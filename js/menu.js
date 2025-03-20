b5219429ad1ab9f607c88d15d3dc34b0d35ca78d
site1Stefano2024 <164583765+Le1ndro-Augusto@users.noreply.github.com>
2025-03-19 20:46:06 -0400

    UP

--- js/menu.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const menuToggle = document.getElementById("menu-toggle");
-    const menu = document.querySelector("nav");
-
-    menuToggle.addEventListener("click", function () {
-        menu.classList.toggle("active");
-    });
-});
